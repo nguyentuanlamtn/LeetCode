a97e67a50a96c0ea5790d80461c9ab7473a8c5e5
nguyentuanlamtn <nguyentuanlamtn@gmail.com>
2025-01-08 16:59:25 +0700

    lan 2

---
+++ climbStairs/climbStairs.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int climbStairs(int n) {
+    if (n <= 2) {
+        return n;
+    }
+    int a = 1, b = 2;
+    for (int i = 3; i <= n; i++) {
+        int temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of steps: ";
+    cin >> n;
+
+    cout << "The number of ways to climb to the top is: " << climbStairs(n) << endl;
+    return 0;
+}
